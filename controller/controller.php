8f3d7fd0b0af3bc5c806630409fc00c2d4677928
kevin.huonic <kevininc155@gmail.com>
2019-12-19 15:25:51 +0000

    home

---
+++ controller/controller.php
@@ -0,0 +1,7 @@
+<?php 
+
+require('model/model.php');
+
+function home() {
+    require('view/home.php');
+}
---
+++ view/home.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Pokéducation</title>
+</head>
+    <body>
+        <header>
+            <div class="header">
+                <nav class = "navbar navbar-expand-lg justify-content-center container-fullwidth sticky-top mr-auto ml-auto" style="position:absolute;"> 
+                    <!-- <img class="navbar-brand" src="ressources/pokeball.png" alt=""> -->
+                    <ul class="nav">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Histoire</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Générations</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Médias</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Pop-culture</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Quizz</a>
+                        </li>
+                    </ul>
+                </nav>
+                
+
+                <div class="embed-responsive embed-responsive-16by9">
+                    <video autoplay muted loop>
+                        <source src="ressources/homepage.mp4" type="video/mp4">
+                    </video>
+                </div>
+            </div>
+        </header>
+
+        <footer class="font-small ">
+            <div class=" text-center py-3 links " >
+                <img src ="ressources/logo-pokemon-company.svg" alt="img nintendo">
+                <br>
+                <br>
+                <img src ="ressources/logo-nintendo.svg" alt="img nintendo">
+                <br>
+                <br>
+                <a href="https://www.pokemon.com/fr/conditions-d-utilisation/">
+                Conditions d'utilisation
+                </a> |
+                <a href="https://www.pokemon.com/fr/avis-de-confidentialite/"> 
+                Avis de confidentialité
+                </a> |
+                <a href="https://www.pokemon.com/fr/juridique/"> Informations juridiques</a>
+                <br>
+                <br>
+                <text>©2019 Pokémon. ©1995–2019 Nintendo / Creatures Inc. / GAME FREAK inc. Pokémon et Nintendo Switch sont des marques de Nintendo. ©2019 Nintendo.
+                </text>
+            </div>
+        </footer>
+    </body>
+</html>
